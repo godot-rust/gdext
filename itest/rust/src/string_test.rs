e5492392d9f2ea0fae89b6712b536c50e0c53ffe
Jan Haller <bromeon@gmail.com>
2022-06-10 23:00:45 +0200

    Remove String/StringName relocation tests
    Returning pointer to local variable is fundamentally unsound, and turning the String/StringName structs into a "value is sys pointer" approach has other issues (like constructing in-place). The current approach follows godot-cpp, so should be good to go.

--- itest/rust/src/string_test.rs
+++ itest/rust/src/string_test.rs
@@ -1,6 +1,6 @@
 use crate::godot_itest;
 use gdext_builtin::{GodotString, StringName};
-use gdext_sys::{self as sys, interface_fn};
+use gdext_sys::{self as sys};
 
 // TODO use tests from godot-rust/gdnative
 
@@ -10,10 +10,8 @@ pub fn run() -> bool {
     ok &= string_equality();
     ok &= string_ordering();
     ok &= string_clone();
-    ok &= string_relocate();
     ok &= string_name_conversion();
     ok &= string_name_default_construct();
-    ok &= string_name_relocate();
     ok
 }
 
@@ -51,19 +49,6 @@ godot_itest! { string_clone {
     assert_eq!(first, cloned);
 }}
 
-#[inline(never)]
-fn make_string_ptr() -> sys::GDNativeStringPtr {
-    let stack_allocated = GodotString::from("some string");
-    stack_allocated.leak_string_sys()
-}
-
-godot_itest! { string_relocate {
-    let ptr = make_string_ptr();
-    let reconstructed = unsafe { GodotString::from_string_sys(ptr) };
-
-    assert_eq!(reconstructed, GodotString::from("some string"));
-}}
-
 // ----------------------------------------------------------------------------------------------------------------------------------------------
 
 godot_itest! { string_name_conversion {
@@ -80,32 +65,3 @@ godot_itest! { string_name_default_construct {
 
     assert_eq!(back, GodotString::new());
 }}
-
-fn make_string_name() -> StringName {
-    StringName::from(&GodotString::from("some string"))
-}
-
-#[inline(never)]
-fn make_string_name_ptr() -> sys::GDNativeStringNamePtr {
-    let stack_allocated = make_string_name();
-    stack_allocated.leak_string_sys()
-}
-
-godot_itest! { string_name_relocate {
-    let ptr = make_string_name_ptr();
-    let reconstructed = unsafe { StringName::from_string_sys(ptr) };
-    let back = GodotString::from(&reconstructed);
-
-    assert_eq!(back, GodotString::from("some string"));
-}}
-
-// fn string_relocated() {
-//     let first = MaybeUninit::new();
-//     let second = MaybeUninit::uninit();
-//
-//     unsafe {
-//         let first_ptr = first.assume_init_ref().string_sys();
-//         interface_fn!(string_to_utf8_chars)(first_ptr)
-//     }
-//     assert_eq!(string, back);
-// }}
