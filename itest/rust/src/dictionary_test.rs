f15d60665954e4be525faab1c80b830e904ab3c6
Lili Zoey <lili.andersen@nrk.no>
2023-01-29 04:56:54 +0100

    Add dictionary iterators

--- itest/rust/src/dictionary_test.rs
+++ itest/rust/src/dictionary_test.rs
@@ -6,7 +6,7 @@
 
 use std::collections::{HashMap, HashSet};
 
-use crate::itest;
+use crate::{expect_panic, itest};
 use godot::{
     builtin::{dict, Dictionary, FromVariant, ToVariant},
     prelude::{Share, Variant},
@@ -19,8 +19,6 @@ pub fn run() -> bool {
     ok &= dictionary_from_iterator();
     ok &= dictionary_from();
     ok &= dictionary_macro();
-    ok &= dictionary_try_to_hashmap();
-    ok &= dictionary_try_to_hashset();
     ok &= dictionary_clone();
     ok &= dictionary_duplicate_deep();
     ok &= dictionary_hash();
@@ -35,6 +33,15 @@ pub fn run() -> bool {
     ok &= dictionary_contains_keys();
     ok &= dictionary_keys_values();
     ok &= dictionary_equal();
+    ok &= dictionary_iter();
+    ok &= dictionary_iter_equals_big();
+    ok &= dictionary_iter_insert();
+    ok &= dictionary_iter_insert_after_completion();
+    ok &= dictionary_iter_big();
+    ok &= dictionary_iter_simultaneous();
+    ok &= dictionary_iter_panics();
+    ok &= dictionary_iter_clear();
+    ok &= dictionary_iter_erase();
     ok
 }
 
@@ -114,38 +121,6 @@ fn dictionary_macro() {
 }
 
 #[itest]
-fn dictionary_try_to_hashmap() {
-    let dictionary = dict! {
-        "foo": 0,
-        "bar": 1,
-        "baz": 2
-    };
-
-    assert_eq!(
-        HashMap::<String, i64>::try_from(&dictionary),
-        Ok(HashMap::from([
-            ("foo".into(), 0),
-            ("bar".into(), 1),
-            ("baz".into(), 2)
-        ]))
-    );
-}
-
-#[itest]
-fn dictionary_try_to_hashset() {
-    let dictionary = dict! {
-        "foo": true,
-        "bar": true,
-        "baz": true
-    };
-
-    assert_eq!(
-        HashSet::<String>::try_from(&dictionary),
-        Ok(HashSet::from(["foo".into(), "bar".into(), "baz".into()]))
-    );
-}
-
-#[itest]
 fn dictionary_clone() {
     let subdictionary = dict! {
         "baz": true,
@@ -157,9 +132,7 @@ fn dictionary_clone() {
     };
     #[allow(clippy::redundant_clone)]
     let clone = dictionary.share();
-    Dictionary::try_from_variant(&clone.get("bar").unwrap())
-        .unwrap()
-        .insert("final", 4);
+    Dictionary::from_variant(&clone.get("bar").unwrap()).insert("final", 4);
     assert_eq!(subdictionary.get("final"), Some(4.to_variant()));
 }
 
@@ -184,9 +157,7 @@ fn dictionary_duplicate_deep() {
         "bar": subdictionary.share()
     };
     let clone = dictionary.duplicate_deep();
-    Dictionary::try_from_variant(&clone.get("bar").unwrap())
-        .unwrap()
-        .insert("baz", 4);
+    Dictionary::from_variant(&clone.get("bar").unwrap()).insert("baz", 4);
     assert_eq!(
         subdictionary.get("baz"),
         Some(true.to_variant()),
@@ -205,9 +176,7 @@ fn dictionary_duplicate_shallow() {
         "bar": subdictionary.share()
     };
     let mut clone = dictionary.duplicate_shallow();
-    Dictionary::try_from_variant(&clone.get("bar").unwrap())
-        .unwrap()
-        .insert("baz", 4);
+    Dictionary::from_variant(&clone.get("bar").unwrap()).insert("baz", 4);
     assert_eq!(
         subdictionary.get("baz"),
         Some(4.to_variant()),
@@ -260,17 +229,19 @@ fn dictionary_insert() {
 
     assert_eq!(dictionary.insert("bar", 2), Some(1.to_variant()));
     assert_eq!(
-        HashMap::<String, i64>::try_from(&dictionary),
-        Ok(HashMap::from([("foo".into(), 0), ("bar".into(), 2)]))
+        dictionary
+            .iter_shared()
+            .typed::<String, i64>()
+            .collect::<HashMap<_, _>>(),
+        HashMap::from([("foo".into(), 0), ("bar".into(), 2)])
     );
     assert_eq!(dictionary.insert("baz", 3), None);
     assert_eq!(
-        HashMap::<String, i64>::try_from(&dictionary),
-        Ok(HashMap::from([
-            ("foo".into(), 0),
-            ("bar".into(), 2),
-            ("baz".into(), 3)
-        ]))
+        dictionary
+            .iter_shared()
+            .typed::<String, i64>()
+            .collect::<HashMap<_, _>>(),
+        HashMap::from([("foo".into(), 0), ("bar".into(), 2), ("baz".into(), 3)])
     );
 }
 
@@ -387,9 +358,9 @@ fn dictionary_keys_values() {
         "bar": true,
     };
 
-    assert_eq!(dictionary.keys(), Array::from(&["foo", "bar"]));
+    assert_eq!(dictionary.keys_array(), Array::from(&["foo", "bar"]));
     assert_eq!(
-        dictionary.values(),
+        dictionary.values_array(),
         Array::from(&[0.to_variant(), true.to_variant()])
     );
 }
@@ -397,6 +368,259 @@ fn dictionary_keys_values() {
 #[itest]
 fn dictionary_equal() {
     assert_eq!(dict! {"foo": "bar"}, dict! {"foo": "bar"});
-    assert_eq!(dict! {1: f32::NAN}, dict! {1: f32::NAN}); // yes apparently godot considers these equal
+    assert_eq!(dict! {1: f32::NAN}, dict! {1: f32::NAN}); // yes apparently Godot considers these equal
     assert_ne!(dict! {"foo": "bar"}, dict! {"bar": "foo"});
+}
+
+#[itest]
+fn dictionary_iter() {
+    let dictionary = dict! {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": Variant::nil(),
+    };
+
+    let map = HashMap::<String, Variant>::from([
+        ("foo".into(), 0.to_variant()),
+        ("bar".into(), true.to_variant()),
+        ("baz".into(), "foobar".to_variant()),
+        ("nil".into(), Variant::nil()),
+    ]);
+
+    let map2: HashMap<String, Variant> = dictionary.iter_shared().typed().collect();
+    assert_eq!(map, map2);
+}
+
+#[itest]
+fn dictionary_iter_equals_big() {
+    let dictionary: Dictionary = (0..1000).zip(0..1000).collect();
+    let map: HashMap<i64, i64> = (0..1000).zip(0..1000).collect();
+    let collected_map: HashMap<i64, i64> = dictionary.iter_shared().typed::<i64, i64>().collect();
+    assert_eq!(map, collected_map);
+    let collected_dictionary: Dictionary = collected_map.into_iter().collect();
+    assert_eq!(dictionary, collected_dictionary);
+}
+
+// Insertion mid-iteration seems to work and is not explicitly forbidden in the docs:
+// https://docs.godotengine.org/en/latest/classes/class_dictionary.html#description
+
+#[itest]
+fn dictionary_iter_insert() {
+    let dictionary = dict! {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": Variant::nil(),
+    };
+    let mut dictionary2 = dictionary.share();
+
+    let mut iter = dictionary.iter_shared();
+    iter.next();
+    iter.next();
+
+    dictionary2.insert("new_key", 10);
+    let v: Vec<_> = iter.collect();
+    assert_eq!(dictionary.len(), 5);
+    assert!(dictionary.contains_key("new_key"));
+    assert_eq!(v.len(), 3);
+    assert!(v.contains(&("new_key".to_variant(), 10.to_variant())));
+}
+
+#[itest]
+fn dictionary_iter_insert_after_completion() {
+    let dictionary = dict! {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": Variant::nil(),
+    };
+    let mut dictionary2 = dictionary.share();
+    let mut iter = dictionary.iter_shared();
+    for _ in 0..4 {
+        iter.next();
+    }
+    assert_eq!(iter.next(), None);
+
+    dictionary2.insert("new_key", 10);
+    assert_eq!(iter.next(), None);
+    assert_eq!(dictionary.len(), 5);
+}
+
+#[itest]
+fn dictionary_iter_big() {
+    let dictionary: Dictionary = (0..256).zip(0..256).collect();
+    let mut dictionary2 = dictionary.share();
+    let mut iter = dictionary.iter_shared();
+
+    for _ in 0..4 {
+        for _ in 0..4 {
+            for _ in 0..16 {
+                iter.next();
+            }
+            dictionary2.insert("a", "b");
+        }
+        dictionary2.clear();
+        dictionary2.extend((0..64).zip(0..64));
+    }
+    assert_eq!(dictionary2, (0..64).zip(0..64).collect());
+}
+
+#[itest]
+fn dictionary_iter_simultaneous() {
+    let dictionary = dict! {
+        "foo": 10,
+        "bar": true,
+        "baz": "foobar",
+        "nil": Variant::nil(),
+    };
+
+    let map: HashMap<String, (Variant, Variant)> = dictionary
+        .iter_shared()
+        .typed::<String, Variant>()
+        .zip(dictionary.iter_shared().typed::<String, Variant>())
+        .map(|((mut k1, v1), (k2, v2))| {
+            k1.push_str(k2.as_str());
+            (k1, (v1, v2))
+        })
+        .collect();
+
+    assert!(map.len() == 4);
+
+    let mut tens = 0;
+    let mut trues = 0;
+    let mut foobars = 0;
+    let mut nils = 0;
+
+    for v in map.iter().flat_map(|(_, (v1, v2))| [v1, v2]) {
+        if let Ok(b) = bool::try_from_variant(v) {
+            assert!(b);
+            trues += 1;
+        } else if let Ok(i) = i64::try_from_variant(v) {
+            assert_eq!(i, 10);
+            tens += 1;
+        } else if let Ok(s) = String::try_from_variant(v) {
+            assert_eq!(s.as_str(), "foobar");
+            foobars += 1;
+        } else {
+            assert!(v.is_nil());
+            nils += 1;
+        }
+    }
+
+    assert_eq!(tens, 2);
+    assert_eq!(trues, 2);
+    assert_eq!(foobars, 2);
+    assert_eq!(nils, 2);
+}
+
+#[itest]
+fn dictionary_iter_panics() {
+    expect_panic(
+        "Dictionary containing integer keys should not be convertible to a HashSet<String>",
+        || {
+            let dictionary: Dictionary = (0..10).zip(0..).collect();
+            let _set: HashSet<String> = dictionary.keys_shared().typed::<String>().collect();
+        },
+    );
+
+    expect_panic(
+        "Dictionary containing integer entries should not be convertible to a HashMap<String,String>",
+        || {
+            let dictionary: Dictionary = (0..10).zip(0..).collect();
+            let _set: HashMap<String,String> = dictionary.iter_shared().typed::<String,String>().collect();
+        },
+    );
+}
+
+// The below tests erase entries mid-iteration. This is not supported by Godot dictionaries
+// however it shouldn't cause unsafety or panicking. Rather the outcome of the iteration is not
+// guaranteed. These tests therefore test two main things:
+// 1. The dictionary is not corrupted by erasing mid-iteration.
+// 2. Our implementation behaves the same as Godot.
+//
+// #2 may change in the future, so equivalent GDScript code is provided. That way we can
+// easily check if a failure is a false negative caused by Godot's behavior changing.
+
+#[itest]
+fn dictionary_iter_clear() {
+    let dictionary = dict! {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": Variant::nil(),
+    };
+    let mut dictionary2 = dictionary.share();
+
+    let mut iter = dictionary.iter_shared();
+    iter.next();
+    iter.next();
+    dictionary2.clear();
+    let v: Vec<_> = iter.collect();
+    assert!(dictionary.is_empty(), "Dictionary contains {dictionary:?}.");
+    assert!(v.is_empty(), "Vec contains {v:?}.");
+    /* equivalent GDScript code:
+    ```
+    var dictionary = {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": null,
+    }
+    var arr = []
+
+    var i = 0
+    for key in dictionary:
+        var value = dictionary.get(key)
+        if i == 1:
+            dictionary.clear()
+        elif i > 1:
+            arr.append([key, value])
+        i += 1
+    print(dictionary)
+    print(arr)
+    ```
+     */
+}
+
+#[itest]
+fn dictionary_iter_erase() {
+    let dictionary = dict! {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": Variant::nil(),
+    };
+    let mut dictionary2 = dictionary.share();
+
+    let mut iter = dictionary.iter_shared();
+    iter.next();
+    iter.next();
+    dictionary2.remove("baz");
+    let v: Vec<_> = iter.collect();
+    assert_eq!(dictionary.len(), 3);
+    assert_eq!(v.len(), 1);
+    assert!(v.contains(&("nil".to_variant(), Variant::nil())));
+    /* equivalent GDScript code:
+    ```
+    var dictionary = {
+        "foo": 0,
+        "bar": true,
+        "baz": "foobar",
+        "nil": null,
+    }
+    var arr = []
+
+    var i = 0
+    for key in dictionary:
+        var value = dictionary.get(key)
+        if i == 1:
+            dictionary.erase("baz")
+        elif i > 1:
+            arr.append([key, value])
+        i += 1
+    print(dictionary)
+    print(arr)
+    ```
+     */
 }
