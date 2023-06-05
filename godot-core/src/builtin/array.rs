godot-rust/gdext

commit fc4a405a2c4400a333c60d226cc5eacf1f008342

Remove several memory leaks by constructing into uninitialized pointers

godot-core/src/builtin/array.rs
@@ -716,13 +716,10 @@ impl<T: VariantMetadata> FromVariant for Array<T> {
             return Err(VariantConversionError);
         }
 
-        // TODO(uninit): can we use from_sys_init()? Godot uses placement-new for variant_to_type initialization.
-        use sys::AsUninit;
-
         let array = unsafe {
-            Self::from_sys_init_default(|self_ptr| {
+            Self::from_sys_init(|self_ptr| {
                 let array_from_variant = sys::builtin_fn!(array_from_variant);
-                array_from_variant(self_ptr.as_uninit(), variant.var_sys());
+                array_from_variant(self_ptr, variant.var_sys());
             })
         };
 

godot-core/src/builtin/callable.rs
@@ -10,7 +10,6 @@ use crate::builtin::{inner, ToVariant, Variant};
 use crate::engine::Object;
 use crate::obj::mem::Memory;
 use crate::obj::{Gd, GodotClass, InstanceId};
-use godot_ffi::AsUninit;
 use std::fmt;
 use sys::{ffi_methods, GodotFfi};
 
@@ -46,11 +45,10 @@ impl Callable {
         // upcast not needed
         let method = method_name.into();
         unsafe {
-            // TODO(uninit): can we use from_sys_init()? Godot uses placement-new for variant_to_type initialization.
-            Self::from_sys_init_default(|self_ptr| {
+            Self::from_sys_init(|self_ptr| {
                 let ctor = sys::builtin_fn!(callable_from_object_method);
                 let args = [object.sys_const(), method.sys_const()];
-                ctor(self_ptr.as_uninit(), args.as_ptr());
+                ctor(self_ptr, args.as_ptr());
             })
         }
     }

godot-core/src/builtin/string/godot_string.rs
@@ -222,13 +222,11 @@ impl FromStr for GodotString {
 
 impl From<&StringName> for GodotString {
     fn from(string: &StringName) -> Self {
-        // TODO(uninit) - see if we can use from_sys_init()
-        use godot_ffi::AsUninit;
         unsafe {
-            Self::from_sys_init_default(|self_ptr| {
+            Self::from_sys_init(|self_ptr| {
                 let ctor = sys::builtin_fn!(string_from_string_name);
                 let args = [string.sys_const()];
-                ctor(self_ptr.as_uninit(), args.as_ptr());
+                ctor(self_ptr, args.as_ptr());
             })
         }
     }
@@ -245,13 +243,11 @@ impl From<StringName> for GodotString {
 
 impl From<&NodePath> for GodotString {
     fn from(path: &NodePath) -> Self {
-        // TODO(uninit) - see if we can use from_sys_init()
-        use godot_ffi::AsUninit;
         unsafe {
-            Self::from_sys_init_default(|self_ptr| {
+            Self::from_sys_init(|self_ptr| {
                 let ctor = sys::builtin_fn!(string_from_node_path);
                 let args = [path.sys_const()];
-                ctor(self_ptr.as_uninit(), args.as_ptr());
+                ctor(self_ptr, args.as_ptr());
             })
         }
     }

godot-core/src/builtin/string/node_path.rs
@@ -100,13 +100,11 @@ impl_rust_string_conv!(NodePath);
 
 impl From<&GodotString> for NodePath {
     fn from(string: &GodotString) -> Self {
-        // TODO(uninit) - see if we can use from_sys_init()
-        use godot_ffi::AsUninit;
         unsafe {
-            Self::from_sys_init_default(|self_ptr| {
+            Self::from_sys_init(|self_ptr| {
                 let ctor = sys::builtin_fn!(node_path_from_string);
                 let args = [string.sys_const()];
-                ctor(self_ptr.as_uninit(), args.as_ptr());
+                ctor(self_ptr, args.as_ptr());
             })
         }
     }

godot-core/src/builtin/string/string_name.rs
@@ -121,7 +121,6 @@ impl fmt::Debug for StringName {
         write!(f, "&\"{string}\"")
     }
 }
-
 // ----------------------------------------------------------------------------------------------------------------------------------------------
 // Conversion from/into other string-types
 
@@ -129,13 +128,11 @@ impl_rust_string_conv!(StringName);
 
 impl From<&GodotString> for StringName {
     fn from(string: &GodotString) -> Self {
-        // TODO(uninit) - see if we can use from_sys_init()
-        use godot_ffi::AsUninit;
         unsafe {
-            Self::from_sys_init_default(|self_ptr| {
+            Self::from_sys_init(|self_ptr| {
                 let ctor = sys::builtin_fn!(string_name_from_string);
                 let args = [string.sys_const()];
-                ctor(self_ptr.as_uninit(), args.as_ptr());
+                ctor(self_ptr, args.as_ptr());
             })
         }
     }

godot-core/src/builtin/variant/impls.rs
@@ -61,18 +61,15 @@ macro_rules! impl_variant_traits {
                     return Err(VariantConversionError)
                 }
 
-            	// TODO(uninit) - see if we can use from_sys_init()
-                use ::godot_ffi::AsUninit;
-
                 // In contrast to T -> Variant, the conversion Variant -> T assumes
                 // that the destination is initialized (at least for some T). For example:
                 // void String::operator=(const String &p_str) { _cowdata._ref(p_str._cowdata); }
                 // does a copy-on-write and explodes if this->_cowdata is not initialized.
                 // We can thus NOT use Self::from_sys_init().
                 let result = unsafe {
-                    Self::from_sys_init_default(|self_ptr| {
+                    Self::from_sys_init(|self_ptr| {
                         let converter = sys::builtin_fn!($to_fn);
-                        converter(self_ptr.as_uninit(), variant.var_sys());
+                        converter(self_ptr, variant.var_sys());
                     })
                 };
 

godot-core/src/builtin/variant/mod.rs
@@ -108,23 +108,7 @@ impl Variant {
         let args_sys: Vec<_> = args.iter().map(|v| v.var_sys_const()).collect();
         let mut error = sys::default_call_error();
 
-        #[allow(unused_mut)]
-        let mut result = Variant::nil();
-
-        use sys::AsUninit;
-        unsafe {
-            interface_fn!(variant_call)(
-                self.var_sys(),
-                method.string_sys(),
-                args_sys.as_ptr(),
-                args_sys.len() as i64,
-                result.var_sys().as_uninit(),
-                ptr::addr_of_mut!(error),
-            )
-        };
-
-        // TODO(uninit)
-        /*let result = unsafe {
+        let result = unsafe {
             Variant::from_var_sys_init(|variant_ptr| {
                 interface_fn!(variant_call)(
                     self.var_sys(),
@@ -135,7 +119,7 @@ impl Variant {
                     ptr::addr_of_mut!(error),
                 )
             })
-        };*/
+        };
 
         if error.error != sys::GDEXTENSION_CALL_OK {
             let arg_types: Vec<_> = args.iter().map(Variant::get_type).collect();
@@ -148,21 +132,7 @@ impl Variant {
         let op_sys = op.sys();
         let mut is_valid = false as u8;
 
-        use sys::AsUninit;
-        #[allow(unused_mut)]
-        let mut result = Variant::nil();
-        unsafe {
-            interface_fn!(variant_evaluate)(
-                op_sys,
-                self.var_sys(),
-                rhs.var_sys(),
-                result.var_sys().as_uninit(),
-                ptr::addr_of_mut!(is_valid),
-            )
-        };
-
-        // TODO(uninit)
-        /*let mut result = unsafe {
+        let result = unsafe {
             Variant::from_var_sys_init(|variant_ptr| {
                 interface_fn!(variant_evaluate)(
                     op_sys,
@@ -172,7 +142,7 @@ impl Variant {
                     ptr::addr_of_mut!(is_valid),
                 )
             })
-        };*/
+        };
 
         if is_valid == 1 {
             Some(result)

godot-core/src/builtin/macros.rs
@@ -11,17 +11,11 @@ macro_rules! impl_builtin_traits_inner {
         impl Default for $Type {
             #[inline]
             fn default() -> Self {
-                // TODO(uninit): use from_sys_init()
-                let mut uninit = std::mem::MaybeUninit::<$Type>::uninit();
-
-                use godot_ffi::AsUninit;
                 unsafe {
-                    let self_ptr = (*uninit.as_mut_ptr()).sys_mut().as_uninit();
-                    sys::builtin_call! {
-                        $gd_method(self_ptr, std::ptr::null_mut())
-                    };
-
-                    uninit.assume_init()
+                    Self::from_sys_init(|self_ptr| {
+                        let ctor = ::godot_ffi::builtin_fn!($gd_method);
+                        ctor(self_ptr, std::ptr::null_mut())
+                    })
                 }
             }
         }
@@ -31,13 +25,11 @@ macro_rules! impl_builtin_traits_inner {
         impl Clone for $Type {
             #[inline]
             fn clone(&self) -> Self {
-                // TODO(uninit) - check if we can use from_sys_init()
-                use godot_ffi::AsUninit;
                 unsafe {
-                    Self::from_sys_init_default(|self_ptr| {
+                    Self::from_sys_init(|self_ptr| {
                         let ctor = ::godot_ffi::builtin_fn!($gd_method);
                         let args = [self.sys_const()];
-                        ctor(self_ptr.as_uninit(), args.as_ptr());
+                        ctor(self_ptr, args.as_ptr());
                     })
                 }
             }

commit ede9046f8d2d7e9104680514ef8cb29145a924f3

Re-introduce from_sys_init_default() for API 4.0
Addresses a regression introduced in fc4a405a2c4400a333c60d226cc5eacf1f008342:
> Remove several memory leaks by constructing into uninitialized pointers

godot-core/src/builtin/array.rs
@@ -717,7 +717,7 @@ impl<T: VariantMetadata> FromVariant for Array<T> {
         }
 
         let array = unsafe {
-            Self::from_sys_init(|self_ptr| {
+            sys::from_sys_init_or_init_default::<Self>(|self_ptr| {
                 let array_from_variant = sys::builtin_fn!(array_from_variant);
                 array_from_variant(self_ptr, variant.var_sys());
             })

godot-core/src/builtin/callable.rs
@@ -45,7 +45,7 @@ impl Callable {
         // upcast not needed
         let method = method_name.into();
         unsafe {
-            Self::from_sys_init(|self_ptr| {
+            sys::from_sys_init_or_init_default::<Self>(|self_ptr| {
                 let ctor = sys::builtin_fn!(callable_from_object_method);
                 let args = [object.sys_const(), method.sys_const()];
                 ctor(self_ptr, args.as_ptr());

godot-core/src/builtin/string/godot_string.rs
@@ -223,7 +223,7 @@ impl FromStr for GodotString {
 impl From<&StringName> for GodotString {
     fn from(string: &StringName) -> Self {
         unsafe {
-            Self::from_sys_init(|self_ptr| {
+            sys::from_sys_init_or_init_default::<Self>(|self_ptr| {
                 let ctor = sys::builtin_fn!(string_from_string_name);
                 let args = [string.sys_const()];
                 ctor(self_ptr, args.as_ptr());
@@ -244,7 +244,7 @@ impl From<StringName> for GodotString {
 impl From<&NodePath> for GodotString {
     fn from(path: &NodePath) -> Self {
         unsafe {
-            Self::from_sys_init(|self_ptr| {
+            sys::from_sys_init_or_init_default::<Self>(|self_ptr| {
                 let ctor = sys::builtin_fn!(string_from_node_path);
                 let args = [path.sys_const()];
                 ctor(self_ptr, args.as_ptr());

godot-core/src/builtin/string/node_path.rs
@@ -101,7 +101,7 @@ impl_rust_string_conv!(NodePath);
 impl From<&GodotString> for NodePath {
     fn from(string: &GodotString) -> Self {
         unsafe {
-            Self::from_sys_init(|self_ptr| {
+            sys::from_sys_init_or_init_default::<Self>(|self_ptr| {
                 let ctor = sys::builtin_fn!(node_path_from_string);
                 let args = [string.sys_const()];
                 ctor(self_ptr, args.as_ptr());

godot-core/src/builtin/string/string_name.rs
@@ -129,7 +129,7 @@ impl_rust_string_conv!(StringName);
 impl From<&GodotString> for StringName {
     fn from(string: &GodotString) -> Self {
         unsafe {
-            Self::from_sys_init(|self_ptr| {
+            sys::from_sys_init_or_init_default::<Self>(|self_ptr| {
                 let ctor = sys::builtin_fn!(string_name_from_string);
                 let args = [string.sys_const()];
                 ctor(self_ptr, args.as_ptr());

godot-core/src/builtin/variant/impls.rs
@@ -110,6 +110,11 @@ macro_rules! impl_variant_traits {
     };
 
     (@@from_sys_init, $from_sys_init:ident) => {
+        // let $from_sys_init = Self::from_sys_init;
+
+        #[cfg(gdextension_api = "4.0")]
+        let $from_sys_init = Self::from_sys_init_default;
+        #[cfg(not(gdextension_api = "4.0"))]
         let $from_sys_init = Self::from_sys_init;
     };
 }

godot-core/src/builtin/variant/mod.rs
@@ -108,6 +108,26 @@ impl Variant {
         let args_sys: Vec<_> = args.iter().map(|v| v.var_sys_const()).collect();
         let mut error = sys::default_call_error();
 
+        // #[cfg(gdextension_api = "4.0")]
+        // let result = {
+        //     #[allow(unused_mut)]
+        //     let mut result = Variant::nil();
+        //
+        //     use sys::AsUninit;
+        //     unsafe {
+        //         interface_fn!(variant_call)(
+        //             self.var_sys(),
+        //             method.string_sys(),
+        //             args_sys.as_ptr(),
+        //             args_sys.len() as i64,
+        //             result.var_sys().as_uninit(),
+        //             ptr::addr_of_mut!(error),
+        //         )
+        //     };
+        //     result
+        // };
+        //
+        // #[cfg(not(gdextension_api = "4.0"))]
         let result = unsafe {
             Variant::from_var_sys_init(|variant_ptr| {
                 interface_fn!(variant_call)(
@@ -209,6 +229,16 @@ impl Variant {
         fn from_var_sys_init = from_sys_init;
         fn var_sys = sys;
     }
+
+    // #[doc(hidden)]
+    // pub unsafe fn from_var_sys_init_default(
+    //     init_fn: impl FnOnce(sys::GDExtensionVariantPtr),
+    // ) -> Self {
+    //     #[allow(unused_mut)]
+    //     let mut variant = Variant::nil();
+    //     init_fn(variant.var_sys());
+    //     variant
+    // }
 
     #[doc(hidden)]
     pub fn var_sys_const(&self) -> sys::GDExtensionConstVariantPtr {

godot-ffi/src/godot_ffi.rs
@@ -96,6 +96,32 @@ pub unsafe trait GodotFfi {
     unsafe fn move_return_ptr(self, dst: sys::GDExtensionTypePtr, call_type: PtrcallType);
 }
 
+// In Godot 4.0.x, a lot of that are "constructed into" require a default-initialized value.
+// In Godot 4.1+, placement new is used, requiring no prior value.
+// This method abstracts over that. Outside of GodotFfi because it should not be overridden.
+
+/// # Safety
+///
+/// See [`GodotFfi::from_sys_init`] and [`GodotFfi::from_sys_init_default`].
+#[cfg(gdextension_api = "4.0")]
+pub unsafe fn from_sys_init_or_init_default<T: GodotFfi>(
+    init_fn: impl FnOnce(sys::GDExtensionTypePtr),
+) -> T {
+    T::from_sys_init_default(init_fn)
+}
+
+/// # Safety
+///
+/// See [`GodotFfi::from_sys_init`] and [`GodotFfi::from_sys_init_default`].
+#[cfg(not(gdextension_api = "4.0"))]
+pub unsafe fn from_sys_init_or_init_default<T: GodotFfi>(
+    init_fn: impl FnOnce(sys::GDExtensionUninitializedTypePtr),
+) -> T {
+    T::from_sys_init(init_fn)
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+
 /// Marks a type as having a nullable counterpart in Godot.
 ///
 /// This trait primarily exists to implement GodotFfi for `Option<Gd<T>>`, which is not possible
@@ -114,13 +140,6 @@ unsafe impl<T> GodotFfi for Option<T>
 where
     T: GodotNullablePtr,
 {
-    fn sys(&self) -> sys::GDExtensionTypePtr {
-        match self {
-            Some(value) => value.sys(),
-            None => ptr::null_mut() as sys::GDExtensionTypePtr,
-        }
-    }
-
     unsafe fn from_sys(ptr: sys::GDExtensionTypePtr) -> Self {
         ptr_then(ptr, |ptr| T::from_sys(ptr))
     }
@@ -130,6 +149,13 @@ where
         init_fn(raw.as_mut_ptr() as sys::GDExtensionUninitializedTypePtr);
 
         Self::from_sys(raw.assume_init())
+    }
+
+    fn sys(&self) -> sys::GDExtensionTypePtr {
+        match self {
+            Some(value) => value.sys(),
+            None => ptr::null_mut() as sys::GDExtensionTypePtr,
+        }
     }
 
     unsafe fn from_arg_ptr(ptr: sys::GDExtensionTypePtr, call_type: PtrcallType) -> Self {
@@ -142,6 +168,8 @@ where
         }
     }
 }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 /// An indication of what type of pointer call is being made.
 #[derive(Default, Copy, Clone, Eq, PartialEq, Debug)]

godot-ffi/src/lib.rs
@@ -32,7 +32,9 @@ use std::ffi::CStr;
 #[doc(hidden)]
 pub use paste;
 
-pub use crate::godot_ffi::{GodotFfi, GodotFuncMarshal, GodotNullablePtr, PtrcallType};
+pub use crate::godot_ffi::{
+    from_sys_init_or_init_default, GodotFfi, GodotFuncMarshal, GodotNullablePtr, PtrcallType,
+};
 pub use gen::central::*;
 pub use gen::gdextension_interface::*;
 pub use gen::interface::*;