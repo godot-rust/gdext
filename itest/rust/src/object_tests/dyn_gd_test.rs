cdc5e0453bd58d3f7ca7e585de3c0435de077094
Jan Haller <bromeon@gmail.com>
2024-07-28 18:15:40 +0200

    dyn_gd macro

--- itest/rust/src/object_tests/dyn_gd_test.rs
+++ itest/rust/src/object_tests/dyn_gd_test.rs
@@ -35,13 +35,46 @@ impl<T> Clone for TypeCapsule<T> {
 }
 impl<T> Copy for TypeCapsule<T> {}
 
+macro_rules! dyn_gd_obj {
+    ($Trait:ident; $user_obj:expr) => {
+        dyn_gd!($Trait; Gd::from_object($user_obj))
+    }
+}
+
+macro_rules! dyn_gd {
+    ($Trait:ident; $gd_instance:expr) => {{
+        let gd = $gd_instance;
+
+        // Without the explicit type annotation, we get the weird error:
+        // error[E0308]: mismatched types
+        //    |
+        // 59 |         DynGd::<Thing, dyn Health>::new(gd, downcast)
+        //    |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ one type is more general than the other
+        //
+        // We can also not extract the closure into a separate variable, it needs to be inline in Box::new(...).
+        //
+        // Furthermore, we should be able to store this in a fn pointer without Box, however rustc
+        // doesn't tolerate captures (type_), *even if they are ZSTs*.
+        let downcast: Box<dyn Fn(&mut Gd<Object>) -> DynGdMut<_, dyn $Trait>> =
+            Box::new(|obj: &mut Gd<Object>| {
+                let concrete: &mut Gd<_> = unsafe { std::mem::transmute(obj) };
+
+                let guard = guard(concrete, /*type_*/);
+
+                DynGdMut::from_guard(guard, |t: &mut _| -> &mut dyn $Trait { t })
+            });
+
+        DynGd::<_, dyn $Trait>::new(gd, downcast)
+    }}
+}
+
 #[allow(dead_code)]
 fn test() {
     let user_obj = Thing {};
     // let d = dyn_gd!(Health; node);
 
-    let type_ = TypeCapsule::new(&user_obj);
-    let mut dyn_gd = {
+    // let type_ = TypeCapsule::new(&user_obj);
+    /*let mut dyn_gd = {
         let gd = Gd::from_object(user_obj);
 
         // let type_ = type_.clone();
@@ -67,7 +100,11 @@ fn test() {
             });
 
         DynGd::<_, dyn Health>::new(gd, downcast)
-    };
+    };*/
+
+    // let mut dyn_gd = dyn_gd!(Health; user_obj);
+    let mut dyn_gd = dyn_gd!(Health; Gd::from_object(user_obj));
+
     let t = dyn_gd.dbind_mut();
     let _ = t;
 }
