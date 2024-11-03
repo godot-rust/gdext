0af7118f6919981b9a10c6fab7036b0a4358d7ed
Jan Haller <bromeon@gmail.com>
2024-11-03 23:06:40 +0100

    Enable `register-docs` feature for unit test/clippy

--- godot/tests/docs.rs
+++ godot/tests/docs.rs
@@ -1,10 +1,12 @@
-#![cfg(feature = "register-docs")]
 /*
  * Copyright (c) godot-rust; Bromeon and contributors.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
+#![cfg(feature = "register-docs")]
+
 use godot::prelude::*;
 
 /// *documented* ~ **documented** ~ [AABB] < [pr](https://github.com/godot-rust/gdext/pull/748)
