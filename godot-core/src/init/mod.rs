f0485c1a79b2c477d11169cf0d002c4fb2ac0f1c
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2025-02-10 20:18:37 +0100

    Global main thread ID

--- godot-core/src/init/mod.rs
+++ godot-core/src/init/mod.rs
@@ -16,6 +16,9 @@ use crate::out;
 
 pub use sys::GdextBuild;
 
+#[cfg(not(wasm_nothreads))]
+pub use sys::main_thread_id;
+
 #[doc(hidden)]
 #[deny(unsafe_op_in_unsafe_fn)]
 pub unsafe fn __gdext_load_library<E: ExtensionLibrary>(
