570a4218dc9dfb844f9f6b450a3a151e43c45d3c
Jan Haller <bromeon@gmail.com>
2025-03-08 12:38:50 +0100

    auth token

--- .github/other/download-artifact.sh
+++ .github/other/download-artifact.sh
@@ -17,7 +17,7 @@ outFilename="$3"
 echo "Download artifact: $workflowFile > $artifactName..."
 
 # Find latest **successful** workflow run for the specified workflow file.
-workflowRunId=$(curl -s -H "Accept: application/vnd.github+json" \
+workflowRunId=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "https://api.github.com/repos/$REPO/actions/workflows/$workflowFile/runs?status=success&per_page=1" | \
   jq -r ".workflow_runs[0].id")
 
@@ -29,7 +29,7 @@ fi
 echo "Latest successful run ID for workflow '$workflowFile' is $workflowRunId."
 
 # List artifacts for that run.
-artifactsJson=$(curl -s -H "Accept: application/vnd.github+json" \
+artifactsJson=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "https://api.github.com/repos/$REPO/actions/runs/$workflowRunId/artifacts")
 
 # Find artifact by filtering name.
@@ -45,7 +45,7 @@ fi
 echo "Found artifact '$artifactName' in run $workflowRunId."
 
 # Download the artifact by following the redirect URL.
-curl --fail-with-body -L -o "$outFilename" "$downloadUrl" || {
+curl --fail-with-body -L -o "$outFilename" "$downloadUrl" -H "Authorization: Bearer ${GITHUB_TOKEN}" || {
   # When failed, print response body.
   cat "$outFilename"
   echo "::error::Failed to download artifact '$artifactName' from workflow '$workflowFile'."
