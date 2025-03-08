f7ab6db547cbe6aad8d41cf00013220aa158b380
Jan Haller <bromeon@gmail.com>
2025-03-08 12:40:01 +0100

    only auth final download

--- .github/other/download-artifact.sh
+++ .github/other/download-artifact.sh
@@ -17,7 +17,7 @@ outFilename="$3"
 echo "Download artifact: $workflowFile > $artifactName..."
 
 # Find latest **successful** workflow run for the specified workflow file.
-workflowRunId=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+workflowRunId=$(curl -s -H "Accept: application/vnd.github+json"  \
   "https://api.github.com/repos/$REPO/actions/workflows/$workflowFile/runs?status=success&per_page=1" | \
   jq -r ".workflow_runs[0].id")
 
@@ -29,7 +29,7 @@ fi
 echo "Latest successful run ID for workflow '$workflowFile' is $workflowRunId."
 
 # List artifacts for that run.
-artifactsJson=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+artifactsJson=$(curl -s -H "Accept: application/vnd.github+json" \
   "https://api.github.com/repos/$REPO/actions/runs/$workflowRunId/artifacts")
 
 # Find artifact by filtering name.
