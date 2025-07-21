fce77d68f247ef1b038bdfea5e7bc28efbd4a855
Sílvia Elisabeth Romero García <silviae.romgar@gmail.com>
2025-07-21 14:26:22 +0200

    implement mut bindings for match_class

--- godot-core/src/classes/match_class.rs
+++ godot-core/src/classes/match_class.rs
@@ -35,7 +35,8 @@
 ///
 /// // More diverse dispatch patterns are also supported.
 /// let fancy_dispatch: i32 = match_class! { some_input(),
-///     button @ InputEventMouseButton => 1,
+///     // Mutable bindings supported:
+///     mut button @ InputEventMouseButton => 1,
 ///
 ///     // Block syntax for multiple statements:
 ///     motion @ InputEventMouseMotion => {
@@ -48,6 +49,8 @@
 ///
 ///     // Fallback with variable -- retrieves original Gd<InputEvent>.
 ///     original => 0,
+///     // Can also be used with mut:
+///     // mut original => 0,
 /// };
 ///
 /// // event_type is now 0, 1, 2, or 3
@@ -73,13 +76,24 @@ macro_rules! match_class {
 #[macro_export]
 macro_rules! match_class_muncher {
     /*
-    // If we want to support `var @ _ => { ... }` syntax, use this branch first (to not match `_` as type).
+    // If we want to support `variable @ _ => { ... }` syntax, use this branch first (to not match `_` as type).
     ($subject:ident, $var:ident @ _ => $block:expr $(,)?) => {{
         let $var = $subject;
         $block
     }};
      */
 
+    // mut variable @ Class => { ... }.
+    ($subject:ident, mut $var:ident @ $Ty:ty => $block:expr, $($rest:tt)*) => {{
+        match $subject.try_cast::<$Ty>() {
+            Ok(mut $var) => $block,
+            Err(__obj) => {
+                $crate::match_class_muncher!(__obj, $($rest)*)
+            }
+        }
+    }};
+
+    // variable @ Class => { ... }.
     ($subject:ident, $var:ident @ $Ty:ty => $block:expr, $($rest:tt)*) => {{
         match $subject.try_cast::<$Ty>() {
             Ok($var) => $block,
@@ -89,11 +103,19 @@ macro_rules! match_class_muncher {
         }
     }};
 
+    // mut variable => { ... }.
+    ($subject:ident, mut $var:ident => $block:expr $(,)?) => {{
+        let mut $var = $subject;
+        $block
+    }};
+
+    // variable => { ... }.
     ($subject:ident, $var:ident => $block:expr $(,)?) => {{
         let $var = $subject;
         $block
     }};
 
+    // _ => { ... }.
     ($subject:ident, _ => $block:expr $(,)?) => {{
         $block
     }};
