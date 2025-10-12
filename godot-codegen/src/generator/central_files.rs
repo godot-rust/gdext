f24ad1cd14ad0b84b38489a2daf2f25a2052e334
Yarvin <yavinlecretin@gmail.com>
2025-10-12 11:28:18 +0200

    Codegen: Support sys types in outgoing Ptrcalls.
    Post-CR fixes (merged ASAP because it was blocking CI + nightly while generated code was alright).
    
    - Don't explicitly declare abstraction for Sys Pointer Types.
    - Simplify execution.

--- godot-codegen/src/generator/central_files.rs
+++ godot-codegen/src/generator/central_files.rs
@@ -10,7 +10,7 @@ use quote::{format_ident, quote, ToTokens};
 
 use crate::context::Context;
 use crate::conv;
-use crate::generator::sys::make_godotconvert_for_systypes;
+use crate::generator::sys_pointer_types::make_godotconvert_for_systypes;
 use crate::generator::{enums, gdext_build_struct};
 use crate::models::domain::ExtensionApi;
 use crate::util::ident;
@@ -61,7 +61,7 @@ pub fn make_core_central_code(api: &ExtensionApi, ctx: &mut Context) -> TokenStr
 
     let (global_enum_defs, global_reexported_enum_defs) = make_global_enums(api);
     let variant_type_traits = make_variant_type_enum(api, false);
-    let sys_types_godotconvert_impl = make_godotconvert_for_systypes();
+    let sys_types_godotconvert_impl = make_godotconvert_for_systypes(ctx);
 
     // TODO impl Clone, Debug, PartialEq, PartialOrd, Hash for VariantDispatch
     // TODO could use try_to().unwrap_unchecked(), since type is already verified. Also directly overload from_variant().
@@ -124,7 +124,10 @@ pub fn make_core_central_code(api: &ExtensionApi, ctx: &mut Context) -> TokenStr
             #( #global_reexported_enum_defs )*
         }
 
-        #( #sys_types_godotconvert_impl )*
+        pub mod sys_pointer_types {
+            use crate::sys;
+            #( #sys_types_godotconvert_impl )*
+        }
     }
 }
 
